a0414fb7dd2021650534f7bfc2777238a3106442
BimoChan07 <bmochanb@gmail.com>
2023-07-22 23:02:02 +0545

    navbar component added

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+// Navbar.js
+
+import React, { useState } from "react";
+import "./Navbar.css"; // Import your custom CSS file for styling
+
+const Navbar = () => {
+  // State to manage the visibility of the mobile menu
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  // Function to toggle the mobile menu
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">Your Logo</div>
+      <div className={`navbar-links ${isMobileMenuOpen ? "active" : ""}`}>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+        {/* Add more navigation links as needed */}
+      </div>
+      <div className="navbar-mobile-toggle" onClick={toggleMobileMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
