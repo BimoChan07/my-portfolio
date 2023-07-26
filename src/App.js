6532fcd32596738adab3c1248c58718acf1263ca
BimoChan07 <bmochanb@gmail.com>
2023-07-26 19:50:09 +0545

    homepage linked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Homepage />
     </div>
   );
 };
